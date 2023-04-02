d52bfbde1e702de0339b0ac44c5bff8139e27fcb
mdaugavietis <martins.daugavietis@edu.rtu.lv>
2023-04-02 22:13:20 +0300

    Uzdevums izpildits

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # python3
+# 3.10
+from typing import Optional, TypeVar
 
 class Query:
     def __init__(self, query):
@@ -7,39 +9,109 @@ class Query:
         if self.type == 'add':
             self.name = query[2]
 
+
+Contact = tuple[int, str]
+oContact = Optional[Contact]
+ContactNode = TypeVar("Self", bound="ContactNode")
+oContactNode = Optional[ContactNode]
+class ContactNode:
+    def __init__(self, value: Contact):
+        self.prev: oContactNode = None
+        self.next: oContactNode = None
+        self.value: Contact = value
+
+
+class Bucket:
+    def __init__(self):
+        self.head: oContactNode = None
+        self.tail: oContactNode = None
+
+    def add(self, contact: Contact) -> None:
+        if (self.head is None):
+            self.head = ContactNode(contact)
+            self.tail = self.head
+        else:
+            n = self.head
+            while True:
+                if n.value[0] == contact[0]:
+                    n.value = contact
+                    return
+                if n.next is None:
+                    n.next = ContactNode(contact)
+                    self.tail = n.next
+                    return
+                n = n.next
+
+    def getName(self, number: int) -> str:
+        n = self.head
+        while True:
+            if n.value[0] == number:
+                return n.value[1]
+            if n.next is None:
+                return "not found" 
+            n = n.next
+
+    def remove(self, number: int) -> None:
+        n = self.head
+        while True:
+            if n.value[0] == number:
+                if n is self.head:
+                    self.head = n.next
+                    return
+                elif n is self.tail:
+                    n.prev = None
+                    return
+                else:
+                    n.prev.next, n.next.prev= n.next, n.prev
+                    return
+            if n.next is None:
+                return
+            n = n.next
+
+
+class PhoneBook:
+    def __init__(self, m: int):
+        self.buckets: list[Bucket] = [Bucket()] * m
+        self.m: int = m
+
+    def hashNumber(self, number: int) -> int:
+        return ((518396941*number+86950909)%374219533)%self.m
+
+    def add(self, contact: Contact):
+        self.buckets[self.hashNumber(contact[0])].add(contact)
+
+    def find(self, number: int) -> str:
+        return self.buckets[self.hashNumber(number)].getName(number)
+
+    def delete(self, number: int) -> None:
+        self.buckets[self.hashNumber(number)].remove(number)
+
+
 def read_queries():
     n = int(input())
     return [Query(input().split()) for i in range(n)]
 
+
 def write_responses(result):
     print('\n'.join(result))
 
+
+# add - add(key, value)
+# del - remove(key)
+# find - get(key)
 def process_queries(queries):
     result = []
     # Keep list of all existing (i.e. not deleted yet) contacts.
-    contacts = []
+    contacts = PhoneBook(50)
     for cur_query in queries:
         if cur_query.type == 'add':
             # if we already have contact with such number,
             # we should rewrite contact's name
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    contact.name = cur_query.name
-                    break
-            else: # otherwise, just add it
-                contacts.append(cur_query)
+            contacts.add((cur_query.number, cur_query.name))
         elif cur_query.type == 'del':
-            for j in range(len(contacts)):
-                if contacts[j].number == cur_query.number:
-                    contacts.pop(j)
-                    break
+            contacts.delete(cur_query.number)
         else:
-            response = 'not found'
-            for contact in contacts:
-                if contact.number == cur_query.number:
-                    response = contact.name
-                    break
-            result.append(response)
+            result.append(contacts.find(cur_query.number))
     return result
 
 if __name__ == '__main__':
